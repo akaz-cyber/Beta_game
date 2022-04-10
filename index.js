3ddc99329fe375b3a7dbf6d8f6640c02a8ea4710
akaz-cyber <ajrisonic62@gmail.com>
2022-04-10 11:40:33 +0700

    movement

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ class Player{
         this.width,this.height)
    }
    update(){
-    this.draw()   
+    this.draw()
+    this.posisi.x += this.kecepatan.x    
     this.posisi.y += this.kecepatan.y 
 
     if(this.posisi.y + this.height + this.kecepatan.y <= canvas.height)  
@@ -33,10 +34,67 @@ class Player{
    }
 }
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left:{
+        pressed: false
+    },
+}
 
 function animasi() {
     requestAnimationFrame(animasi)
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+    if(keys.right.pressed){
+        player.kecepatan.x = 5 
+    } else if (keys.left.pressed){
+        player.kecepatan.x = -5
+    } else player.kecepatan.x = 0
 }
 animasi()
+
+addEventListener('keydown', ({keyCode}) => {
+//   console.log(keyCode)
+  switch (keyCode){
+      case 65:
+          console.log('left')
+          keys.left.pressed = true
+          break
+    case 87:
+        console.log('up')
+        player.kecepatan.y -= 20
+        break
+     case 68:
+        console.log('right')
+        keys.right.pressed = true
+        break
+    case 83:
+        console.log('down')
+        break    
+  }
+  console.log(keys.right.pressed)
+})
+
+addEventListener('keyup', ({keyCode}) => {
+    //   console.log(keyCode)
+      switch (keyCode){
+          case 65:
+              console.log('left')
+              keys.left.pressed = false
+              break
+        case 87:
+            console.log('up')
+            player.kecepatan.y -= 20
+            break
+         case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break
+        case 83:
+            console.log('down')
+            break    
+      }
+      console.log(keys.right.pressed)
+    })
